53592d3289f5589920ce268ea2d5e0b7fcaf52db
Khairul Thamrin <choo_gi@yahoo.com>
2013-11-25 11:28:00 +0700

    commit

--- src/main/java/com/khairul/controller/FlightController.java
+++ src/main/java/com/khairul/controller/FlightController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
-
+ 
 import com.khairul.model.Flight;
 import com.khairul.model.FlightSchedule;
 import com.khairul.model.Route;
